1503d0ebbd42f57ae29b4b4b1e6ba806a3bf8fdb
Jean matos <matos.sd52@gmail.com>
2025-10-15 22:48:09 -0300

    Criação do escopo do projeto e tratamento de erros

---
+++ src/main/java/ort/serratec/atividade/individual/enums/GeneroMusical.java
@@ -0,0 +1,19 @@
+package ort.serratec.atividade.individual.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import ort.serratec.atividade.individual.exception.EnumValidationException;
+
+public enum GeneroMusical {
+    ROCK,POP,SAMBA,FUNK,SERTANEJO;
+
+    @JsonCreator
+    public static GeneroMusical Verifica(String value) throws EnumValidationException {
+
+        for (GeneroMusical generoMusical: values()) {
+            if (value.equals(generoMusical.name())) {
+                return generoMusical;
+            }
+        }
+        throw  new EnumValidationException("Genero musical válidos: ROCK,POP,SAMBA,FUNK,SERTANEJO");
+    }
+}
